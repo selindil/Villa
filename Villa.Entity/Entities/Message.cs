fe42e77624b78c470d55298f4428f774ea2b3333
selindil <0selindil@gmail.com>
2024-11-24 00:02:42 +0300

    ...

---
+++ Villa.Entity/Entities/Message.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Villa.Entity.Entities
+{
+    public class Message : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Subjecr { get; set; }
+        public string MessageCount { get; set; }
+    }
+}
