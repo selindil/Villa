dcaf039212826cd5decdc248e18f4e4822d3c4bd
selindil <0selindil@gmail.com>
2024-11-24 00:02:42 +0300

    ...

---
+++ Villa.Entity/Entities/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Villa.Entity.Entities
+{
+    public class Product : BaseEntity
+    {
+        public string ImageUrl { get; set; }
+        public string Category {  get; set; }
+        public string Price {  get; set; }
+        public string Title {  get; set; }
+        public int BedroomCount {  get; set; }
+        public string BathroomCount {  get; set; }
+        public string Area {  get; set; }
+        public string Floor {  get; set; }
+        public string ParkingCount {  get; set; }
+    }
+}
