2103c66c3ed6223b142a7b918ff528d55147adb7
selindil <0selindil@gmail.com>
2024-12-16 18:44:10 +0300

    ...

--- Villa.Business/Concrete/BannerManager.cs
+++ Villa.Business/Concrete/BannerManager.cs
@@ -1,12 +1,52 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
+using Villa.Business.Abstract;
+using Villa.DataAccess.Abstract;
 
 namespace Villa.Business.Concrete
 {
-    internal class BannerManager
+    public class BannerManager : IBannerService
     {
+        private readonly IBannerDal _bannerDal;
+        public async Task<int> TCountAsync()
+        {
+            return await _bannerDal.CountAsync();
+        }
+
+        public async Task TCreateAsync(Banner entity)
+        {
+             await _bannerDal.CreateAsync(entity);
+        }
+
+        public async Task TDeleteAsync(ObjectId id)
+        {
+            await _bannerDal.DeleteAsync(id);
+        }
+
+        public async Task<Banner> TGetByIdAsync(ObjectId id)
+        {
+            return await _bannerDal.GetByIdAsync(id);
+        }
+
+        public async Task<List<Banner>> TGetFilteredListAsync(Expression<Func<Banner, bool>> predicate)
+        {
+            return await _bannerDal.GetFilteredListAsync(predicate);
+        }
+
+        public async Task<List<Banner>> TGetListAsync()
+        {
+            return await _bannerDal.GetListAsync();
+        }
+
+        public async Task TUpdateAsync(Banner entity)
+        {
+            await _bannerDal.UpdateAsync(entity);
+        }
     }
 }
